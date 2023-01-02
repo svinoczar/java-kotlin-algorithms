61e1ddd259a450d08cc532c455f2a2b04e9a27d9
notsunshine <alexsupper75@gmail.com>
2023-01-02 23:54:32 +0300

    [MODIFY] refactor bubbleTest and add selectionTest

--- src/main/java/sorted/SortedTests.java
+++ src/main/java/sorted/SortedTests.java
@@ -1,21 +1,36 @@
 package sorted;
 
 public class SortedTests {
-
-
     public static void bubbleTest (int[] array){
-        System.out.println("\n---------------------------------------");
+//        System.out.println("---------------------------------------");
         // Вывод неотсортированного тестового массива.
         for (int i : array) {
             System.out.print(i + ", ");
         }
         System.out.println();
-
         // Проверка сортировки пузырьком.
         Sorted.bubbleSort(array);
         for (int i : array) {
             System.out.print(i + ", ");
         }
-        System.out.println("\n---------------------------------------");
+        System.out.println();
+//        System.out.println("\n---------------------------------------");
+    }
+
+
+    public static void selectionTest (int[] array){
+//        System.out.println("\n---------------------------------------");
+        // Вывод неотсортированного тестового массива.
+        for (int i : array) {
+            System.out.print(i + ", ");
+        }
+        System.out.println();
+        // Проверка сортировки выбором.
+        Sorted.bubbleSort(array);
+        for (int i : array) {
+            System.out.print(i + ", ");
+        }
+        System.out.println();
+//        System.out.println("\n---------------------------------------");
     }
 }
