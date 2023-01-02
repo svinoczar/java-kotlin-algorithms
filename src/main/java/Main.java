42447ea0e9883503e095dba9087049eda00f49ff
notsunshine <alexsupper75@gmail.com>
2023-01-02 22:59:48 +0300

    [MODIFY] add 5 test arrays and 5 bubbleSort tests

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,17 +1,19 @@
 import sorted.Sorted;
+import sorted.SortedTests;
 
 public class Main {
     public static void main(String[] args) {
-        int[] arr = {9,8,7,6,5,4,3,2,1};
-        for (int i = 0; i < arr.length; i++){
-            System.out.print(arr[i] + ", ");
-        }
+        int[] arr1 = {9,8,7,6,5,4,3,2,1};
+        int[] arr2 = {11,12,10,13,9,14,8,15,7};
+        int[] arr3 = {5,5,4,4,3,3,2,2,1,0};
+        int[] arr4 = {1,2,3,4,5,4,3,2,1};
+        int[] arr5 = {0,0,0,0,7,8,9};
 
-        System.out.println();
-
-        arr = Sorted.bubbleSort(arr);
-        for (int i = 0; i < arr.length; i++){
-            System.out.print(arr[i] + ", ");
-        }
+        // bubbleSort tests 1-5
+        SortedTests.bubbleTest(arr1);
+        SortedTests.bubbleTest(arr2);
+        SortedTests.bubbleTest(arr3);
+        SortedTests.bubbleTest(arr4);
+        SortedTests.bubbleTest(arr5);
     }
 }
