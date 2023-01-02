8ca8db001c3ae5f7dbdbdd2517f758e3f9bbc06e
notsunshine <alexsupper75@gmail.com>
2023-01-02 22:59:02 +0300

    [MODIFY] add bubbleSort test

--- src/main/java/sorted/SortedTests.java
+++ src/main/java/sorted/SortedTests.java
@@ -1,4 +1,21 @@
 package sorted;
 
 public class SortedTests {
+
+
+    public static void bubbleTest (int[] array){
+        System.out.println("\n---------------------------------------");
+        // Вывод неотсортированного тестового массива.
+        for (int i : array) {
+            System.out.print(i + ", ");
+        }
+        System.out.println();
+
+        // Проверка сортировки пузырьком.
+        Sorted.bubbleSort(array);
+        for (int i : array) {
+            System.out.print(i + ", ");
+        }
+        System.out.println("\n---------------------------------------");
+    }
 }
