40654e519d37d22e460a74cd7a3078d469e16a20
notsunshine <alexsupper75@gmail.com>
2023-01-03 16:27:31 +0300

    [MODIFY] refactor selectionTest in SortedTests

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
 
 
-
+        
         System.out.println("\n*** START TESTS ***\n");
 
         SortedTests.bubbleTest(4);
--- src/main/java/sorted/SortedTests.java
+++ src/main/java/sorted/SortedTests.java
@@ -25,18 +25,27 @@ public class SortedTests {
     }
 
 
-    public static void selectionTest (int[] array){
-        // Вывод неотсортированного тестового массива.
-        for (int i : array) {
-            System.out.print(i + ", ");
+    public static void selectionTest(int count) {
+        for (int n = 0; n < count; n++) {
+            System.out.println("\tTEST #" + (n+1));
+            int[] array;
+            int arrLen = (int) (4 + Math.random() * 6);
+            array = new int[arrLen];
+            for (int i = 0; i < arrLen-1; i++) {
+                array[i] = (int) (0 + Math.random() * 10);
+            }
+            // Вывод неотсортированного тестового массива.
+            for (int i : array) {
+                System.out.print(i + ", ");
+            }
+            System.out.println();
+            // Проверка сортировки выбором.
+            Sorted.bubbleSort(array);
+            for (int i : array) {
+                System.out.print(i + ", ");
+            }
+            System.out.println("\n");
         }
-        System.out.println();
-        // Проверка сортировки выбором.
-        Sorted.bubbleSort(array);
-        for (int i : array) {
-            System.out.print(i + ", ");
-        }
-        System.out.println();
     }
 
 
